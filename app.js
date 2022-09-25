36c006d862c59fd15a46ab159598dffe5fd9f4ad
Movaan87 <83492355+Movaan87@users.noreply.github.com>
2022-09-25 14:00:02 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+
+const handleRequest = (request) => {
+  return new Response("This is a practise app");
+};
+
+let port = 7777;
+if (Deno.args.length > 0) {
+  const lastArgument = Deno.args[Deno.args.length - 1];
+  port = Number(lastArgument);
+}
+
+serve(handleRequest, { port: port });
