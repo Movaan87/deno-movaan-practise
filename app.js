57166986c5ec7b5bd3da4bd6588e6d6bbf8764c9
Movaan87 <83492355+Movaan87@users.noreply.github.com>
2022-10-08 12:47:52 +0300

    committed to walking skeleton template

--- app.js
+++
@@ -1,7 +0,0 @@
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
-
-const handleRequest = async (request) => {
-  return Response("Hello world!");
-};
-
-serve(handleRequest, { port: 7777 });
--- flyway/sql/V1___initial_schema.sql
+++ flyway/sql/V1___initial_schema.sql
@@ -8,3 +8,9 @@ CREATE TABLE addresses (
   name TEXT NOT NULL,
   address TEXT NOT NULL
 );
+
+CREATE TABLE messages (
+  id SERIAL PRIMARY KEY,
+  sender TEXT NOT NULL,
+  message TEXT NOT NULL
+)
