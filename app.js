a9921471b63fd9c1a01adb651fe2aa420ef509fd
Movaan87 <83492355+Movaan87@users.noreply.github.com>
2022-10-08 11:56:31 +0300

    Modified APP to Walking Skeleton

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
-const handleRequest = (request) => {
-  return new Response("This is a practise app with Git commit, version 2.0, removed runtime");
+const handleRequest = async (request) => {
+  return Response("Hello world!");
 };
 
-let port = 7777;
-if (Deno.args.length > 0) {
-  const lastArgument = Deno.args[Deno.args.length - 1];
-  port = Number(lastArgument);
-}
-
-serve(handleRequest, { port: port });
+serve(handleRequest, { port: 7777 });
